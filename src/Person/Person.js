47b29c0f61c1ae7de6e98f3ce05022db8d42610e
unknown <batuhan.balaban@yahoo.com>
2019-07-27 14:45:19 +0300

    applied media query styles with help of radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Radium, {StyleRoot} from 'radium'
 import './Person.css'
 
 const person = (props) =>{
+
+const mystyle = {
+    '@media(min-width: 500px)':{
+        width:'450px'
+    }
+
+};
+
+
     return (
-        <div className="Person">
-            <p onClick={props.click}>I'm {props.name} I am {props.age} years old :)</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    
+        <StyleRoot>
+            <div className="Person" style ={mystyle}  >
+                <p onClick={props.click}>I'm {props.name} I am {props.age} years old :)</p>
+                <p>{props.children}</p>
+                <input type="text" onChange={props.changed} value={props.name}/>
+            </div>
+        </StyleRoot>
     )
 };
 
