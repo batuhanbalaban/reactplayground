8515c694a04316e56ece32e90752dc03f78d00a1
unknown <batuhan.balaban@yahoo.com>
2019-08-11 13:23:24 +0300

    using Refs for focus()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,14 @@ import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
 
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount(){
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
 
     render(){
 
@@ -15,7 +23,13 @@ class Person extends Component {
             <Fragment>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} I am {this.props.age} years old :)</p>
                 <p key="i2">{this.props.children}</p>
-                <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input 
+                    //ref={(inputEl)=>{this.inputElement = inputEl;}}
+                    ref={this.inputElementRef}
+                    key="i3" 
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}/>
             </Fragment>
         );
     }
